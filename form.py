46e4d5b76e6f25b276bd35d0bda19107f862cf11
Denis <dionis201290@gmail.com>
2020-11-28 00:35:43 +0200

    Add submition functionality.

--- form.py
+++ form.py
@@ -1,4 +1,5 @@
 import cherrypy
+import sqlite3
 
 
 class WebForm(object):
@@ -8,8 +9,17 @@ class WebForm(object):
             return f"""{html.read()}"""
 
     @cherrypy.expose
-    def submit_form(self, name, OSType):
-        print(f"Form is submitted {name} and {OSType}.")
+    def submit_form(self, name, email, ran_commands, produced_output, file, expected, os_type, has_root):
+        print(f"Form is submitted by {name} and {os_type}.")
+        values = (name, email, ran_commands, produced_output, expected, os_type, has_root)
+        DB = sqlite3.connect("db.db")
+        curr_db_conn = DB.cursor()
+        curr_db_conn.execute("INSERT INTO form_log \
+        (name, email, ran_commands, produced_output, expected, os_type, has_root) VALUES (?, ?, ?, ?, ?, ?, ?)", values)
+        DB.commit()
+        DB.close()
+        with open("screenshots/file", 'w') as f:
+            f.write(file)
 
 
 if __name__ == '__main__':
